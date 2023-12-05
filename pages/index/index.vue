1d5f2a155da365234bfb1edf83a921f08c4c417b
guitarher <15283197636@163.com>
2023-12-05 14:15:05 +0800

    feat: init project

---
+++ pages/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<view class="content">
+		首页
+	</view>
+</template>
+
+<script setup>
+	import { ref } from 'vue'
+	const title = ref('hello world')
+</script>
+
+<style lang="scss" scoped>
+	
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  __name: "index",
+  setup(__props) {
+    common_vendor.ref("hello world");
+    return (_ctx, _cache) => {
+      return {};
+    };
+  }
+};
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "D:/other projects/train-app/pages/index/index.vue"]]);
+wx.createPage(MiniProgramPage);
